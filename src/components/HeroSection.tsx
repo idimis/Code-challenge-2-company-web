bed9327892f89c3cdc1559b4beae401e8a2a37ca
idimis <immanuel.adimas@gmail.com>
2024-10-11 21:58:26 +0700

    tidy up after final layouting

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,37 +5,46 @@ import solarPanelImage from '@/public/solarpanelmeadow.png';
 const HeroSection = () => {
   return (
     <div className="relative flex items-center justify-center overflow-hidden min-h-[300px] md:min-h-[650px]">
+      
       <div className="absolute inset-0 flex items-center justify-center z-0">
         <div className="max-w-[1440px] w-full">
           <Image
             src={solarPanelImage}
             alt="Solar Panel Field"
-            className="w-full h-full object-cover rounded-lg shadow-lg" // Added rounded and shadow classes
+            className="w-full h-full object-cover rounded-lg shadow-lg"
             sizes="(max-width: 768px) 100vw, (min-width: 769px) 50vw"
+            style={{ filter: 'brightness(60%)' }}  
           />
         </div>
       </div>
 
+      
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-center text-white p-5">
-        <div className="mt-5 max-w-md mx-auto"> 
+        <div className="mt-5 max-w-md mx-auto">
           <h1 
-            className="text-xl sm:text-2xl md:text-3xl font-bold mb-2"  
-            style={{ textShadow: '2px 2px 0px black' }} 
+            className="text-xl sm:text-2xl md:text-3xl font-bold mb-2" 
+            style={{ 
+              textShadow: '1px 1px 2px rgba(0, 0, 0, 0.9)',  
+            }}
           >
             Harnessing the Power of Nature
           </h1>
           <p 
-            className="text-sm sm:text-base md:text-lg mb-3"  
+            className="text-sm sm:text-base md:text-lg mb-3" 
+            style={{ 
+              textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8)' 
+            }}
           >
             Join us in creating a sustainable future through renewable energy solutions.
           </p>
           <div className="flex justify-center">
             <a
               href="/about"
-              className="inline-block px-2 py-1 sm:px-3 sm:py-2 bg-transparent border border-white text-white font-semibold rounded hover:bg-gray-200 transition"  
+              className="inline-block px-2 py-1 sm:px-3 sm:py-2 bg-transparent border border-white text-white font-semibold rounded hover:bg-gray-200 transition"
               style={{
-                fontSize: '0.65rem',  
-                minWidth: '100px',    
+                fontSize: '0.65rem',
+                minWidth: '100px',
+                textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8)',  // Menambahkan shadow ke teks link
               }}
             >
               Discover More
