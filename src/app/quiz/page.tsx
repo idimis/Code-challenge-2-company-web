206f069d362e103ead930e28b1261b84aa288804
idimis <immanuel.adimas@gmail.com>
2024-10-19 23:33:29 +0700

    add page stats

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import Header from '@/components/Header';
 
